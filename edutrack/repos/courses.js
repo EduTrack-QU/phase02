7f1834508ddf77ad10112641236f2cd5d0698e7c
hm77a <ha2205541@qu.edu.qa>
2025-04-29 20:04:01 +0300

    The total of students per course stat [done]]

--- edutrack/repos/courses.js
+++ edutrack/repos/courses.js
@@ -1,1 +1,15 @@
 import prisma from './prisma';
+
+
+export async function getToatalStudentPerCourse(courseId) {
+  const totalStudent = await prisma.course.findUnique({
+    where: {
+      id: courseId,
+    },
+    select: {
+      students: true,
+    },
+  });
+  return totalStudent.students.length;
+
+}

a7ecddb8b628609d0762dce84ff02b853f5e1317
ansari <elansari1211@gmail.com>
2025-04-29 23:56:00 +0300

    created empty dashboard

---
+++ edutrack/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+     
+    </div>
+  );
+}

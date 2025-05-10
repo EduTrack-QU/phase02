930976570f87732212e23a96d8af29439287cf1b
BKR-17 <ae2205649@qu.edu.qa>
2025-05-11 02:42:19 +0300

    server actions

--- edutrack/app/action/server-actions.js
+++ edutrack/app/action/server-actions.js
@@ -5,3 +5,15 @@ export async function getAverageGpaAction() {
     const averageGpa = await getAverageGpa();
     return averageGpa;
 }
+export async function getHighestGpaAction() {
+    const highestGpa = await getHighestGpa();
+    return highestGpa;
+}
+export async function getLowGPAStudentsAction() {
+    const lowGPAStudents = await getLowGPAStudents();
+    return lowGPAStudents;
+}
+export async function getDeansListAction() {
+    const deansList = await getDeansList();
+    return deansList;
+}
